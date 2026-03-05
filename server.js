57939bc52c9aa9374bed0a16171af1d2b6ae92e9
Lohith2117 <kaminenilohith@gmail.com>
2026-03-05 18:55:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const cors = require('cors');
+app.use(cors()); // This tells the server: "It's okay to accept requests from my website"
 const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
